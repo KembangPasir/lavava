c3f8366f85c04dc5b5a3ce08b45d0a1fea731010
KembangPasir <82569632+KembangPasir@users.noreply.github.com>
2024-03-26 22:21:59 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-a65f798e-be67-43af-95df-6e8435cba044/)
 
 
 echo "$curl_output" >> Update.tsx
